1e5a6e264f2079931e60551065c857a463f3928f
Jack C <75650821+jcarney2024@users.noreply.github.com>
2023-01-17 01:33:53 +0000

    Fix Blank Page Bug

--- src/CodeInput.js
+++ src/CodeInput.js
@@ -7,11 +7,11 @@ function CodeInput() {
   const [codeSnippet, setCodeSnippet] = useState('');
   const [instructions, setInstructions] = useState('');
   const [response, setResponse] = useState('');
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
-    setLoading(true);
+    setIsLoading(true);
     axios.post('https://api.openai.com/v1/edits', {
       instruction: instructions,
       input: codeSnippet,
@@ -25,44 +25,32 @@ function CodeInput() {
       .then((res) => {
         let text = res.data.choices[0].text;
         setResponse(text);
-        setLoading(false);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        setLoading(false);
+        setIsLoading(false);
       });
   }
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label>Code Snippet</Form.Label>
-              <Form.Control as="textarea" value={codeSnippet} onChange={(event) => setCodeSnippet(event.target.value)} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Instructions</Form.Label>
-              <Form.Control as="textarea" rows="3" value={instructions} onChange={(event) => setInstructions(event.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit" disabled={isLoading}>
-              {isLoading ? <Spinner
-                as="span"
-                animation="border"
-                size="sm"
-                role="status"
-                aria-hidden="true"
-              /> : 'Submit'}
-            </Button>
-            <Form.Group>
-              <Form.Label>Response</Form.Label>
-              <Diff inputA={codeSnippet} inputB={response} type='chars' />
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Code Snippet</Form.Label>
+        <Form.Control as="textarea" rows="5" value={codeSnippet} onChange={(event) => setCodeSnippet(event.target.value)} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Instructions</Form.Label>
+        <Form.Control as="textarea" rows="3" value={instructions} onChange={(event) => setInstructions(event.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={isLoading}>
+        {isLoading ? <Spinner animation="border" size="sm" role="status" aria-hidden="true" /> : 'Submit'}
+      </Button>
+      <Form.Group>
+        <Form.Label>Response</Form.Label>
+        <Diff inputA={codeSnippet} inputB={response} type='chars' />
+      </Form.Group>
+    </Form>
   );
 }
 
