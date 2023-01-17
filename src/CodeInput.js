d04c349b98395faa0accd198ef581ef035fb6c4f
Jack C <75650821+jcarney2024@users.noreply.github.com>
2023-01-17 01:35:54 +0000

    Fix Bootstrap Import Statement

--- src/CodeInput.js
+++ src/CodeInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Form, Button, Container, Row, Col, Spinner } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
 import Diff from 'react-diff';
 
 function CodeInput() {
