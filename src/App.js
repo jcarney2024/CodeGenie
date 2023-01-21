ccd14b16596aa5568af9564103e04ff0cfef9a84
Jack C <75650821+jcarney2024@users.noreply.github.com>
2023-01-21 18:23:54 +0000

    Added a Footer!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import CodeInput from './CodeInput';
+import { FaGithub } from 'react-icons/fa';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <div className="center-content" style={{ paddingTop: "20px"}}>
         <CodeInput />
       </div>
+      <footer>
+        <p>© 2023 CodeGenie</p>
+        <a href="https://github.com/jcarney2024/CodeGenie">
+          <FaGithub /> View this project on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
