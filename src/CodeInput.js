b4f6f035bde92a8ff3918aa519b75cd42981185d
Restyled.io <commits@restyled.io>
2023-01-22 15:42:25 +0000

    Restyled by clang-format

--- src/CodeInput.js
+++ src/CodeInput.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Form, Button, Container, Row, Col, Spinner } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './CodeInput.css';
+
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import React, {useState} from 'react';
+import {Button, Col, Container, Form, Row, Spinner} from 'react-bootstrap';
 import Center from 'react-center';
 
 const containerStyles = {
-  height: '500px',
-  width: '80%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
+  height : '500px',
+  width : '80%',
+  display : 'flex',
+  alignItems : 'center',
+  justifyContent : 'center'
 };
 
 const responseTextBoxStyles = {
-  marginBottom: '10px'
+  marginBottom : '10px'
 };
 
 const formStyles = {
-  marginTop: '50px'
+  marginTop : '50px'
 };
 
 function CodeInput() {
@@ -32,40 +33,40 @@ function CodeInput() {
   function handleSubmit(event) {
     event.preventDefault();
     setLoading(true);
-    axios.post('https://api.openai.com/v1/edits', {
-      instruction: instructions,
-      input: codeSnippet,
-      model: 'code-davinci-edit-001',
-      temperature: 0,
-      top_p: 1,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + process.env.REACT_APP_REPLACE_WITH_YOUR_API_KEY,
-      }
-    })
-      .then((res) => {
-        let text = res.data.choices[0].text;
-        setResponse(text);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
+    axios
+        .post(
+            'https://api.openai.com/v1/edits', {
+              instruction : instructions,
+              input : codeSnippet,
+              model : 'code-davinci-edit-001',
+              temperature : 0,
+              top_p : 1,
+            },
+            {
+              headers : {
+                'Content-Type' : 'application/json',
+                'Authorization' :
+                    'Bearer ' + process.env.REACT_APP_REPLACE_WITH_YOUR_API_KEY,
+              }
+            })
+        .then((res) => {
+          let text = res.data.choices[0].text;
+          setResponse(text);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
   }
 
   function handleCopy() {
     navigator.clipboard.writeText(response)
-      .then(() => {
-        setCopied(true);
-        setTimeout(() => {
-          setCopied(false);
-        }, 2000);
-      })
-      .catch(err => {
-        console.log('Failed to copy text: ', err);
-      });
+        .then(() => {
+          setCopied(true);
+          setTimeout(() => { setCopied(false); }, 2000);
+        })
+        .catch(err => { console.log('Failed to copy text: ', err); });
   }
 
   function handleCopyToCode() {
@@ -87,15 +88,16 @@ function CodeInput() {
                 </Form.Group>
                 <Form.Group className="text-box">
                   <Form.Label style={{ marginTop: '20px' }}>Instructions</Form.Label>
-                  <Form.Control as="textarea" cols={40} rows={6} value={instructions} onChange={(event) => setInstructions(event.target.value)} />
+                  <Form.Control as="textarea" cols={40} rows={6} value={instructions} onChange={
+    (event) => setInstructions(event.target.value)} />
                 </Form.Group>
                 <div className="button-spacing">
                   <Button variant="primary" type="submit" disabled={isLoading}>
                     {isLoading ? <Spinner
                       as="span"
-                      animation="border"
-                      size="sm"
-                      role="status"
+  animation = "border"
+  size = "sm"
+  role = "status"
                       aria-hidden="true"
                     /> : 'Submit'}
                   </Button>
@@ -104,12 +106,14 @@ function CodeInput() {
               <Col xs={6}>
                 <Form.Group className="response-text text-box" style={responseTextBoxStyles}>
                   <Form.Label>Response</Form.Label>
-                  <Form.Control as="textarea" cols={40} rows={15} value={response} onChange={(event) => setResponse(event.target.value)} disabled={true} />
+                  <Form.Control as="textarea" cols={40} rows={15} value={response} onChange={(event) => setResponse(event.target.value)} disabled={
+    true} />
                   {response &&
                     <div>
                       <div className="copy-button">
                         <Button variant="secondary" onClick={handleCopy}>
-                          {copied ? <FontAwesomeIcon icon={faCheck} /> : 'Copy to clipboard'}
+                          {copied ? <FontAwesomeIcon icon={faCheck} /> : 'Copy to clipboard'
+}
                         </Button>
                       </div>
                       <div className="copy-to-code-button">
@@ -125,6 +129,6 @@ function CodeInput() {
       </div>
     </Container>
   );
-}
+                        }
 
-export default CodeInput;
+                        export default CodeInput;
